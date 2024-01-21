f71943952e08d7cdbc9e332e1da2b8340bdc8484
Harrison Chase <hw.chase.17@gmail.com>
2024-01-21 09:03:39 -0800

    agent simulation clean up

--- langgraph/graph/__init__.py
+++ langgraph/graph/__init__.py
@@ -1,4 +1,5 @@
 from langgraph.graph.graph import END, Graph
+from langgraph.graph.message import MessageGraph
 from langgraph.graph.state import StateGraph
 
-__all__ = ["END", "Graph", "StateGraph"]
+__all__ = ["END", "Graph", "StateGraph", "MessageGraph"]
