74df6e55bf263e014081b493c06b5de206668752
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2024-03-20 16:40:42 -0700

    Utility to create tool invocation

--- langgraph/prebuilt/__init__.py
+++ langgraph/prebuilt/__init__.py
@@ -1,6 +1,9 @@
 from langgraph.prebuilt import chat_agent_executor
 from langgraph.prebuilt.agent_executor import create_agent_executor
-from langgraph.prebuilt.tool_executor import ToolExecutor, ToolInvocation
+from langgraph.prebuilt.tool_executor import (
+    ToolExecutor,
+    ToolInvocation,
+)
 
 __all__ = [
     "create_agent_executor",
