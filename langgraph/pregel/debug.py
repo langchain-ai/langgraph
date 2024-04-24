89b5f997b6f5e8cc9bfc2b6528507c9698d28159
Nuno Campos <nuno@langchain.dev>
2024-04-24 14:55:12 -0700

    Improve debug logging

--- langgraph/pregel/debug.py
+++ langgraph/pregel/debug.py
@@ -1,5 +1,6 @@
+from collections import defaultdict
 from pprint import pformat
-from typing import Any, Iterator, Mapping
+from typing import Any, Iterator, Mapping, Sequence
 
 from langchain_core.utils.input import get_bolded_text, get_colored_text
 
@@ -7,22 +8,49 @@ from langgraph.channels.base import BaseChannel, EmptyChannelError
 from langgraph.pregel.types import PregelExecutableTask
 
 
-def print_step_start(step: int, next_tasks: list[PregelExecutableTask]) -> None:
+def print_step_tasks(step: int, next_tasks: list[PregelExecutableTask]) -> None:
     n_tasks = len(next_tasks)
     print(
-        f"{get_colored_text('[langgraph/step]', color='blue')} "
+        f"{get_colored_text(f'[{step}:tasks]', color='blue')} "
         + get_bolded_text(
-            f"Starting step {step} with {n_tasks} task{'s' if n_tasks > 1 else ''}. Next tasks:\n"
+            f"Starting step {step} with {n_tasks} task{'s' if n_tasks > 1 else ''}:\n"
         )
-        + "\n".join(f"- {name}({pformat(val)})" for name, val, _, _, _ in next_tasks)
+        + "\n".join(
+            f"- {get_colored_text(name, 'green')} -> {pformat(val)}"
+            for name, val, _, _, _ in next_tasks
+        )
     )
 
 
-def print_checkpoint(step: int, channels: Mapping[str, BaseChannel]) -> None:
+def print_step_writes(
+    step: int, writes: Sequence[tuple[str, Any]], whitelist: Sequence[str]
+) -> None:
+    by_channel: dict[str, list[Any]] = defaultdict(list)
+    for channel, value in writes:
+        if channel in whitelist:
+            by_channel[channel].append(value)
     print(
-        f"{get_colored_text('[langgraph/checkpoint]', color='blue')} "
-        + get_bolded_text(f"Finishing step {step}. Channel values:\n")
-        + pformat({name: val for name, val in _read_channels(channels)}, depth=3)
+        f"{get_colored_text(f'[{step}:writes]', color='blue')} "
+        + get_bolded_text(
+            f"Finished step {step} with writes to {len(by_channel)} channel{'s' if len(by_channel) > 1 else ''}:\n"
+        )
+        + "\n".join(
+            f"- {get_colored_text(name, 'yellow')} -> {', '.join(pformat(v) for v in vals)}"
+            for name, vals in by_channel.items()
+        )
+    )
+
+
+def print_step_checkpoint(
+    step: int, channels: Mapping[str, BaseChannel], whitelist: Sequence[str]
+) -> None:
+    print(
+        f"{get_colored_text(f'[{step}:checkpoint]', color='blue')} "
+        + get_bolded_text(f"State at the end of step {step}:\n")
+        + pformat(
+            {name: val for name, val in _read_channels(channels) if name in whitelist},
+            depth=3,
+        )
     )
 
 
--- langgraph/pregel/log.py
+++ langgraph/pregel/log.py
@@ -1,3 +1,3 @@
 import logging
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("langgraph")
