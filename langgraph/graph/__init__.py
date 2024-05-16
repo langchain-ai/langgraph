de841a272a644e58e83ddea4bff302d38b92abd5
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2024-05-15 17:52:16 -0700

    Export START

--- langgraph/graph/__init__.py
+++ langgraph/graph/__init__.py
@@ -1,9 +1,10 @@
-from langgraph.graph.graph import END, Graph
+from langgraph.graph.graph import END, Graph, START
 from langgraph.graph.message import MessageGraph, MessagesState, add_messages
 from langgraph.graph.state import StateGraph
 
 __all__ = [
     "END",
+    "START",
     "Graph",
     "StateGraph",
     "MessageGraph",
