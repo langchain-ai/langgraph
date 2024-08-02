8fed5dcec7f6d4715fbcb4269fc260a95147dfa0
vbarda <vadym@langchain.dev>
2024-08-02 11:03:14 -0400

    support classes in the decorator

--- libs/langgraph/langgraph/_api/deprecation.py
+++ libs/langgraph/langgraph/_api/deprecation.py
@@ -1,6 +1,6 @@
 import functools
 import warnings
-from typing import Any, Callable, TypeVar, cast
+from typing import Any, Callable, Type, TypeVar, Union, cast
 
 
 class LangGraphDeprecationWarning(DeprecationWarning):
@@ -8,31 +8,57 @@ class LangGraphDeprecationWarning(DeprecationWarning):
 
 
 F = TypeVar("F", bound=Callable[..., Any])
+C = TypeVar("C", bound=Type[Any])
 
 
 def deprecated(
     since: str, alternative: str, *, removal: str = "", example: str = ""
 ) -> Callable[[F], F]:
-    def decorator(func: F) -> F:
-        @functools.wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> Any:
-            removal_str = removal if removal else "a future version"
-            message = (
-                f"{func.__name__} is deprecated as of version {since} and will be"
-                f" removed in {removal_str}. Use {alternative} instead.{example}"
-            )
-            warnings.warn(message, LangGraphDeprecationWarning, stacklevel=2)
-            return func(*args, **kwargs)
-
-        docstring = (
-            f"**Deprecated**: This function is deprecated as of version {since}. "
-            f"Use `{alternative}` instead."
+    def decorator(obj: Union[F, C]) -> Union[F, C]:
+        removal_str = removal if removal else "a future version"
+        message = (
+            f"{obj.__name__} is deprecated as of version {since} and will be"
+            f" removed in {removal_str}. Use {alternative} instead.{example}"
         )
-        if func.__doc__:
-            docstring = docstring + f"\n\n{func.__doc__}"
-        wrapper.__doc__ = docstring
+        if isinstance(obj, type):
+            original_init = obj.__init__
 
-        return cast(F, wrapper)
+            @functools.wraps(original_init)
+            def new_init(self, *args: Any, **kwargs: Any) -> None:
+                warnings.warn(message, LangGraphDeprecationWarning, stacklevel=2)
+                original_init(self, *args, **kwargs)
+
+            obj.__init__ = new_init
+
+            docstring = (
+                f"**Deprecated**: This class is deprecated as of version {since}. "
+                f"Use `{alternative}` instead."
+            )
+            if obj.__doc__:
+                docstring = docstring + f"\n\n{obj.__doc__}"
+            obj.__doc__ = docstring
+
+            return cast(C, obj)
+        elif callable(obj):
+
+            @functools.wraps(obj)
+            def wrapper(*args: Any, **kwargs: Any) -> Any:
+                warnings.warn(message, LangGraphDeprecationWarning, stacklevel=2)
+                return obj(*args, **kwargs)
+
+            docstring = (
+                f"**Deprecated**: This function is deprecated as of version {since}. "
+                f"Use `{alternative}` instead."
+            )
+            if obj.__doc__:
+                docstring = docstring + f"\n\n{obj.__doc__}"
+            wrapper.__doc__ = docstring
+
+            return cast(F, wrapper)
+        else:
+            raise TypeError(
+                f"Can only add deprecation decorator to classes or callables, got '{type(obj)}' instead."
+            )
 
     return decorator
 
--- libs/langgraph/langgraph/prebuilt/chat_agent_executor.py
+++ libs/langgraph/langgraph/prebuilt/chat_agent_executor.py
@@ -1,4 +1,3 @@
-import types
 from typing import (
     Annotated,
     Callable,
