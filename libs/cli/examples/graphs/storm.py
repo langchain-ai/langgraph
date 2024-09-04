5d2535a659b446da6d76752580ff0e5629ea54eb
vbarda <vadym@langchain.dev>
2024-09-03 21:19:53 -0400

    remove remaining non-langgraph mentions

--- libs/cli/examples/graphs/storm.py
+++ libs/cli/examples/graphs/storm.py
@@ -14,7 +14,7 @@ from langchain_core.messages import (
 )
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 from langchain_core.runnables import RunnableConfig, RunnableLambda
 from langchain_core.runnables import chain as as_runnable
 from langchain_core.tools import tool
