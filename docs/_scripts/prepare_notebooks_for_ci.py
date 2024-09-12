9f41a83590a7d5feee52bb979d0f1011f2d334a3
vbarda <vadym@langchain.dev>
2024-09-11 22:39:52 -0400

    lint

--- docs/_scripts/prepare_notebooks_for_ci.py
+++ docs/_scripts/prepare_notebooks_for_ci.py
@@ -1,23 +1,24 @@
 """Preprocess notebooks for CI. Currently removes pip install cells."""
 
-import os
 import json
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 NOTEBOOK_DIRS = ("docs/docs/how-tos",)
 
+
 def remove_install_cells(notebook_path: str) -> None:
-    with open(notebook_path, 'r') as file:
+    with open(notebook_path, "r") as file:
         notebook = json.load(file)
 
-    for index, cell in enumerate(notebook['cells']):
-        if cell['cell_type'] == 'code':
-            if any('pip install' in line for line in cell['source']):
-                notebook['cells'].pop(index)
+    for index, cell in enumerate(notebook["cells"]):
+        if cell["cell_type"] == "code":
+            if any("pip install" in line for line in cell["source"]):
+                notebook["cells"].pop(index)
                 break  # Exit the loop after removing the cell
 
-    with open(notebook_path, 'w') as file:
+    with open(notebook_path, "w") as file:
         json.dump(notebook, file, indent=2)
 
 
@@ -25,7 +26,7 @@ def process_notebooks() -> None:
     for directory in NOTEBOOK_DIRS:
         for root, _, files in os.walk(directory):
             for file in files:
-                if not file.endswith('.ipynb'):
+                if not file.endswith(".ipynb"):
                     continue
 
                 notebook_path = os.path.join(root, file)
