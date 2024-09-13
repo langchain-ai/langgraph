47831bcd9a89df8d72330542e43bcb659a453df6
vbarda <vadym@langchain.dev>
2024-09-13 11:51:46 -0400

    update script

--- docs/_scripts/execute_notebooks.sh
+++ docs/_scripts/execute_notebooks.sh
@@ -1,5 +1,5 @@
 for file in $(find docs/docs/how-tos -name "*.ipynb" | grep -v ".ipynb_checkpoints")
 do
   echo "Executing $file"
-  jupyter execute "$file"
+  poetry run jupyter execute "$file"
 done
