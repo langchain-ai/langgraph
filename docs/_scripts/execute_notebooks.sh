a0e6c7a88355e9de957ac3f106fa22e80ba13804
isaac hershenson <ihershenson@hmc.edu>
2024-09-17 15:28:02 -0700

    run tutorials

--- docs/_scripts/execute_notebooks.sh
+++ docs/_scripts/execute_notebooks.sh
@@ -1,6 +1,6 @@
 errors=()  # Initialize an array to collect errors
 
-for file in $(find docs/docs/how-tos -name "*.ipynb" | grep -v ".ipynb_checkpoints")
+for file in $(find docs/docs/how-tos docs/docs/tutorials -name "*.ipynb" | grep -v ".ipynb_checkpoints")
 do
   echo "Executing $file"
   if ! output=$(poetry run jupyter execute "$file" 2>&1); then
