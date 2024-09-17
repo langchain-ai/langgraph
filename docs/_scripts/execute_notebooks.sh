02503aa1312337898de0c193d65d4bf67a685738
isaac hershenson <ihershenson@hmc.edu>
2024-09-17 13:18:38 -0700

    actually catch errors

--- docs/_scripts/execute_notebooks.sh
+++ docs/_scripts/execute_notebooks.sh
@@ -1,5 +1,16 @@
-for file in $(find docs/docs/how-tos docs/docs/tutorials -name "*.ipynb" | grep -v ".ipynb_checkpoints")
+errors=()  # Initialize an array to collect errors
+
+for file in $(find docs/docs/how-tos -name "*.ipynb" | grep -v ".ipynb_checkpoints")
 do
   echo "Executing $file"
-  poetry run jupyter execute "$file"
+  if ! output=$(poetry run jupyter execute "$file" 2>&1); then
+    errors+=("$file: $output")  # Add a tuple of the file and error message to the errors list
+  fi
 done
+
+# Optionally, print the errors
+if [ ${#errors[@]} -ne 0 ]; then
+  echo "Errors occurred in the following files:"
+  printf '%s\n' "${errors[@]}"
+  exit 1  # Exit with an error code if there are errors
+fi
