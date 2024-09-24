30ed61479e8d661293ef07c9ba46fbd2ea55eebc
Nuno Campos <nuno@langchain.dev>
2024-09-24 11:06:07 -0700

    Fix

--- libs/sdk-py/langgraph_sdk/client/sync_client.py
+++ libs/sdk-py/langgraph_sdk/client/sync_client.py
@@ -378,7 +378,7 @@ class SyncAssistantsClient:
         """  # noqa: E501
         return self.http.get(f"/assistants/{assistant_id}/schemas")
 
-    async def get_subgraphs(
+    def get_subgraphs(
         self, assistant_id: str, namespace: Optional[str] = None, recurse: bool = False
     ) -> Subgraphs:
         """Get the schemas of an assistant by ID.
@@ -391,12 +391,12 @@ class SyncAssistantsClient:
 
         """  # noqa: E501
         if namespace is not None:
-            return await self.http.get(
+            return self.http.get(
                 f"/assistants/{assistant_id}/subgraphs/{namespace}",
                 params={"recurse": recurse},
             )
         else:
-            return await self.http.get(
+            return self.http.get(
                 f"/assistants/{assistant_id}/subgraphs",
                 params={"recurse": recurse},
             )
