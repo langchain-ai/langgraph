cc59dc9a065017cd84d382839c80e25f5206582c
vbarda <vadym@langchain.dev>
2024-09-26 17:12:46 -0400

    temp

--- docs/_scripts/prepare_notebooks_for_ci.py
+++ docs/_scripts/prepare_notebooks_for_ci.py
@@ -37,7 +37,8 @@ NOTEBOOKS_NO_EXECUTION = [
     "docs/docs/tutorials/rag/langgraph_adaptive_rag.ipynb",
     "docs/docs/tutorials/rag/langgraph_crag.ipynb",
     "docs/docs/tutorials/rag/langgraph_self_rag.ipynb",
-    "docs/docs/tutorials/rag/langgraph_agentic_rag.ipynb"
+    "docs/docs/tutorials/rag/langgraph_agentic_rag.ipynb",
+    "docs/docs/tutorials/plan-and-execute/plan-and-execute.ipynb"  # langsmith issue
 ]
 
 
