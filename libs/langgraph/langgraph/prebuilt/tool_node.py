400693895aa2a9d97a6ab57d3a11904a09ab195d
Bagatur <baskaryan@gmail.com>
2024-10-30 10:51:48 -0700

    langgraph[patch]: fix annotations in inherited tool schemas

--- libs/langgraph/langgraph/prebuilt/tool_node.py
+++ libs/langgraph/langgraph/prebuilt/tool_node.py
@@ -34,6 +34,7 @@ from langchain_core.runnables.config import (
 from langchain_core.runnables.utils import Input
 from langchain_core.tools import BaseTool, InjectedToolArg
 from langchain_core.tools import tool as create_tool
+from langchain_core.tools.base import _get_all_basemodel_annotations
 from typing_extensions import Annotated, get_args, get_origin
 
 from langgraph.errors import GraphInterrupt
@@ -659,7 +660,7 @@ def _get_state_args(tool: BaseTool) -> Dict[str, Optional[str]]:
     full_schema = tool.get_input_schema()
     tool_args_to_state_fields: Dict = {}
 
-    for name, type_ in full_schema.__annotations__.items():
+    for name, type_ in _get_all_basemodel_annotations(full_schema).items():
         injections = [
             type_arg
             for type_arg in get_args(type_)
@@ -683,7 +684,7 @@ def _get_state_args(tool: BaseTool) -> Dict[str, Optional[str]]:
 
 def _get_store_arg(tool: BaseTool) -> Optional[str]:
     full_schema = tool.get_input_schema()
-    for name, type_ in full_schema.__annotations__.items():
+    for name, type_ in _get_all_basemodel_annotations(full_schema).items():
         injections = [
             type_arg
             for type_arg in get_args(type_)
