e1467c27cb15990029de4299a197661d359e4702
Tat Dat Duong <david@duong.cz>
2024-11-04 13:52:40 +0100

    Remove newline

--- libs/cli/langgraph_cli/config.py
+++ libs/cli/langgraph_cli/config.py
@@ -323,8 +323,7 @@ ENV LANGSERVE_GRAPHS='{json.dumps(config["graphs"])}'
 
 WORKDIR {faux_path}
 
-RUN (test ! -f /api/langgraph_api/js/build.mts && echo "Prebuild script not found, skipping") || tsx /api/langgraph_api/js/build.mts
-"""
+RUN (test ! -f /api/langgraph_api/js/build.mts && echo "Prebuild script not found, skipping") || tsx /api/langgraph_api/js/build.mts"""
 
 
 def config_to_docker(config_path: pathlib.Path, config: Config, base_image: str):
