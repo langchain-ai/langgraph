6c4b8922a631862f4f80a7c7047bce4bc04cfcc4
Matt Katz <mattkatz@matts-mbp-2.mynetworksettings.com>
2024-12-11 23:54:02 -0500

    implemented get_writes for postgres. added exists_cached_node in loop. also ttl.

--- libs/langgraph/tests/test_cache.py
+++ libs/langgraph/tests/test_cache.py
@@ -1,7 +1,10 @@
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.postgres import PostgresSaver
 from langgraph.graph import StateGraph, END
 from langgraph.types import CachePolicy
 from typing import TypedDict
+import pytest
+from langchain_core.runnables import RunnableConfig
+
 
 
 
@@ -17,11 +20,7 @@ def test_basic_cache():
     config = {"configurable": {"thread_id": "thread-1"}}
     memory = MemorySaver()
     graph = builder.compile(checkpointer=memory)
-    graph.invoke(1, config) #, debug=True)
-
-    # for elt in list(memory.list(None)):
-    #     print(elt)
-    #     print()
+    graph.invoke(1, config)
 
 def test_dict_cache():
 
@@ -42,8 +41,37 @@ def test_dict_cache():
     config = {"configurable": {"thread_id": "thread-1"}}
     memory = MemorySaver()
     graph = builder.compile(checkpointer=memory)
-    graph.invoke({"foo": 1, "bar": 1}, config) #, debug=True)
+    graph.invoke({"foo": 1, "bar": 1}, config, debug=True)
 
-    for elt in list(memory.list(None)):
-        print(elt)
-        print()
+from langgraph.checkpoint.base import BaseCheckpointSaver
+
+@pytest.mark.parametrize("checkpointer_name", ["postgres"])
+def test_postgres(request: pytest.FixtureRequest, checkpointer_name: str):
+    checkpointer: PostgresSaver = request.getfixturevalue(
+        f"checkpointer_{checkpointer_name}"
+    )
+
+    config = {"configurable": {"thread_id": "thread-1"}}
+
+    class State(TypedDict):
+        foo: int
+        bar: int
+
+    def cache_key(inputs: State, config: RunnableConfig = None):
+        return str(inputs["bar"])
+
+    cache = CachePolicy(cache_key=cache_key)
+    builder = StateGraph(State)
+    builder.add_node("add_two", lambda x: {"foo": x["foo"] + 2}, cache=cache)
+    builder.add_node("subtract_one", lambda x: {"foo": x["foo"] - 1, "bar": (x["bar"] + 1) % 2})
+    builder.add_edge("add_two", "subtract_one")
+    builder.add_conditional_edges("subtract_one", lambda x: END if x["foo"] >= 10 else "add_two")
+    builder.set_entry_point("add_two")
+    
+    graph = builder.compile(checkpointer=checkpointer)
+    graph.invoke({"foo": 1, "bar": 1}, config, debug=True)
+
+    print(checkpointer.get_writes("e5eff38b-2ef4-90a3-f2b2-9ecb227146bb"))
+
+    
+
