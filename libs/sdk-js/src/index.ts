a1dad4360298f3a54c3159c47e86717aed26d7a9
Tat Dat Duong <david@duong.cz>
2025-02-08 20:05:31 -0800

    feat(sdk-js): strongly-typed messages, state/update-type, stream mode

--- libs/sdk-js/src/index.ts
+++ libs/sdk-js/src/index.ts
@@ -26,3 +26,24 @@ export type {
 export { overrideFetchImplementation } from "./singletons/fetch.js";
 
 export type { OnConflictBehavior, Command } from "./types.js";
+
+export type { StreamMode } from "./types.stream.js";
+export type {
+  ValuesPayload,
+  MessagesTuplePayload,
+  MetadataPayload,
+  UpdatesPayload,
+  CustomPayload,
+  MessagesPayload,
+  DebugPayload,
+  EventsPayload,
+} from "./types.stream.js";
+export type {
+  Message,
+  HumanMessage,
+  AIMessage,
+  ToolMessage,
+  SystemMessage,
+  FunctionMessage,
+  RemoveMessage,
+} from "./types.messages.js";
