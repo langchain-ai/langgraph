e94ecfcc06d4a10c69c84ef36e4101bf4a102c64
Eugene Yurtsev <eyurtsev@gmail.com>
2025-02-11 20:24:17 -0500

    x

--- docs/codespell_notebooks.sh
+++ docs/codespell_notebooks.sh
@@ -9,6 +9,16 @@ for file in $(find $1 -name "*.ipynb" | grep -v ".ipynb_checkpoints"); do
     fi
 done
 
+for file in $(find $1 -name "*.md"); do
+    # Adding regexp to ignore base64 strings
+    OUTPUT=$(cat "$file" | codespell --ignore-regex='[A-Za-z0-9+/=]{25,}' -)
+    if [ -n "$OUTPUT" ]; then
+        echo "Errors found in $file"
+        echo "$OUTPUT"
+        ERROR_FOUND=1
+    fi
+done
+
 if [ "$ERROR_FOUND" -ne 0 ]; then
     exit 1
 fi
