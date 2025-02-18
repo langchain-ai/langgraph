9f15e15e269fc5f71567770a47ed738f685d50b0
Eugene Yurtsev <eyurtsev@gmail.com>
2025-02-18 13:33:15 -0500

    reference: remove incorrect example from pregel class (#3476)
    Remove incorrect example from Pregel class. Will follow later this week
    with better docs

--- libs/langgraph/langgraph/pregel/__init__.py
+++ libs/langgraph/langgraph/pregel/__init__.py
@@ -250,25 +250,6 @@ class Pregel(PregelProtocol):
 
     Repeat until no chains are planned for execution, or a maximum number of steps
     is reached.
-
-    Example:
-        ```python
-        from langgraph import Channel, Pregel
-
-        grow_value = (
-            Channel.subscribe_to("value")
-            | (lambda x: x + x)
-            | Channel.write_to(value=lambda x: x if len(x) < 10 else None)
-        )
-
-        app = Pregel(
-            chains={"grow_value": grow_value},
-            input="value",
-            output="value",
-        )
-
-        assert app.invoke("a") == "aaaaaaaa"
-        ```
     """
 
     nodes: dict[str, PregelNode]
