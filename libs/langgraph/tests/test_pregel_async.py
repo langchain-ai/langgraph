c4703bca097ed8efaddf1591c1a6bc33e4861caa
vbarda <vadym@langchain.dev>
2025-03-04 18:16:42 -0500

    update

--- libs/langgraph/tests/test_pregel_async.py
+++ libs/langgraph/tests/test_pregel_async.py
@@ -6148,13 +6148,6 @@ async def test_parent_command(checkpointer_name: str) -> None:
                 "source": "loop",
                 "writes": {
                     "alice": {
-                        "messages": [
-                            _AnyIdHumanMessage(
-                                content="get user name",
-                                additional_kwargs={},
-                                response_metadata={},
-                            ),
-                        ],
                         "user_name": "Meow",
                     }
                 },
