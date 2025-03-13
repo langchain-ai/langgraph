c8992d1b9ab2d556a8f01614417790961617cc85
Chester Curme <chester.curme@gmail.com>
2025-03-13 11:35:46 -0400

    update annotation for tools

--- libs/prebuilt/langgraph/prebuilt/chat_agent_executor.py
+++ libs/prebuilt/langgraph/prebuilt/chat_agent_executor.py
@@ -257,7 +257,7 @@ def _validate_chat_history(
 @_convert_modifier_to_prompt
 def create_react_agent(
     model: Union[str, LanguageModelLike],
-    tools: Union[Sequence[BaseTool], ToolNode],
+    tools: Union[Sequence[Union[BaseTool, Callable]], ToolNode],
     *,
     prompt: Optional[Prompt] = None,
     response_format: Optional[
