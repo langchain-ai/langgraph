a7bcb3299b7c0f6aeae72c6c09de74c0728c1389
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2025-04-10 08:44:26 -0700

    Ensure correct api version is used
    Signed-off-by: William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>

--- libs/cli/langgraph_cli/config.py
+++ libs/cli/langgraph_cli/config.py
@@ -406,7 +406,12 @@ class Config(TypedDict, total=False):
     """
 
 
-PIP_CLEANUP_LINES = """# -- Removing pip from the final image ~<:===~~~ --
+PIP_CLEANUP_LINES = """# -- Ensure user deps didn't inadvertently overwrite langgraph-api
+RUN mkdir -p /api/langgraph_api /api/langgraph_runtime /api/langgraph_license && \
+    touch /api/langgraph_api/__init__.py /api/langgraph_runtime/__init__.py /api/langgraph_license/__init__.py
+RUN PYTHONDONTWRITEBYTECODE=1 pip install --no-cache-dir --no-deps -e /api
+# -- End of ensuring user deps didn't inadvertently overwrite langgraph-api --
+# -- Removing pip from the final image ~<:===~~~ --
 RUN pip uninstall -y pip setuptools wheel && \
     rm -rf /usr/local/lib/python*/site-packages/pip* /usr/local/lib/python*/site-packages/setuptools* /usr/local/lib/python*/site-packages/wheel* && \
     find /usr/local/bin -name "pip*" -delete
