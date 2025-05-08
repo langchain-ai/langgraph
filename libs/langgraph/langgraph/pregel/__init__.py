2f14fbc85f79ff7f02e421710ebbeacaab388516
Nuno Campos <nuno@langchain.dev>
2025-05-08 15:20:58 -0700

    Lint

--- libs/langgraph/langgraph/pregel/__init__.py
+++ libs/langgraph/langgraph/pregel/__init__.py
@@ -552,7 +552,7 @@ class Pregel(PregelProtocol):
         self.config_type = config_type
         self.input_model = input_model
         self.config = config
-        self.trigger_to_nodes = trigger_to_nodes
+        self.trigger_to_nodes = trigger_to_nodes or {}
         self.name = name
         if auto_validate:
             self.validate()
