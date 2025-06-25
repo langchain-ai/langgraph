263a583f01fc31619b8a630898877ea31aec0859
Tat Dat Duong <david@duong.cz>
2025-06-25 02:32:05 +0200

    Fix lint

--- libs/langgraph/langgraph/pregel/__init__.py
+++ libs/langgraph/langgraph/pregel/__init__.py
@@ -219,9 +219,9 @@ class NodeBuilder:
         *channels: str,
     ) -> Self:
         """Adds the specified channels to read from, without subscribing to them."""
-        assert isinstance(
-            self._channels, list
-        ), "Cannot read additional channels when subscribed to single channels"
+        assert isinstance(self._channels, list), (
+            "Cannot read additional channels when subscribed to single channels"
+        )
         self._channels.extend(channels)
         return self
 
