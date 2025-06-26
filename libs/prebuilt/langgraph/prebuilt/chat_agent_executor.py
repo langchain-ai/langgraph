7fda42760106470c4a747a0d1d2a12af729e921d
William FH <13333726+hinthornw@users.noreply.github.com>
2025-06-25 18:34:21 -0700

    fix: [prebuilt] Checks for pre-bound model with builtin tools (#5203)

--- libs/prebuilt/langgraph/prebuilt/chat_agent_executor.py
+++ libs/prebuilt/langgraph/prebuilt/chat_agent_executor.py
@@ -140,7 +140,9 @@ def _get_prompt_runnable(prompt: Optional[Prompt]) -> Runnable:
     return prompt_runnable
 
 
-def _should_bind_tools(model: LanguageModelLike, tools: Sequence[BaseTool]) -> bool:
+def _should_bind_tools(
+    model: LanguageModelLike, tools: Sequence[BaseTool], num_builtin: int = 0
+) -> bool:
     if isinstance(model, RunnableSequence):
         model = next(
             (
@@ -158,9 +160,10 @@ def _should_bind_tools(model: LanguageModelLike, tools: Sequence[BaseTool]) -> b
         return True
 
     bound_tools = model.kwargs["tools"]
-    if len(tools) != len(bound_tools):
+    if len(tools) != len(bound_tools) - num_builtin:
         raise ValueError(
             "Number of tools in the model.bind_tools() and tools passed to create_react_agent must match"
+            f" Got {len(tools)} tools, expected {len(bound_tools) - num_builtin}"
         )
 
     tool_names = set(tool.name for tool in tools)
@@ -444,9 +447,8 @@ def create_react_agent(
     tool_calling_enabled = len(tool_classes) > 0
 
     if (
-        _should_bind_tools(model, tool_classes)
+        _should_bind_tools(model, tool_classes, num_builtin=len(llm_builtin_tools))
         and len(tool_classes) > 0
-        or (len(llm_builtin_tools) > 0)
     ):
         model = cast(BaseChatModel, model).bind_tools(tool_classes + llm_builtin_tools)  # type: ignore[operator]
 
