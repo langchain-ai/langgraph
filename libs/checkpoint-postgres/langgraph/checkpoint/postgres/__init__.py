71d0b0ff737c8e640091320fc46a767a7a4f5008
Sydney Runkle <sydneymarierunkle@gmail.com>
2025-07-01 09:26:34 -0400

    docs and tests

--- libs/checkpoint-postgres/langgraph/checkpoint/postgres/__init__.py
+++ libs/checkpoint-postgres/langgraph/checkpoint/postgres/__init__.py
@@ -284,7 +284,7 @@ class PostgresSaver(BasePostgresSaver):
         configurable = config["configurable"].copy()
         thread_id = configurable.pop("thread_id")
         checkpoint_ns = configurable.pop("checkpoint_ns")
-        checkpoint_id = configurable.pop("checkpoint_id")
+        checkpoint_id = configurable.pop("checkpoint_id", None)
         copy = checkpoint.copy()
         next_config = {
             "configurable": {
