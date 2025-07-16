943980638612e22cf626c0b0b758b7c31e8cabab
Sydney Runkle <sydneymarierunkle@gmail.com>
2025-07-16 19:08:54 -0400

    nicer interface for merge or override

--- libs/langgraph/langgraph/runtime.py
+++ libs/langgraph/langgraph/runtime.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field, replace
 from typing import Any, Generic, cast
+
+from typing_extensions import TypedDict, Unpack
 
 from langgraph._internal._constants import CONF, CONFIG_KEY_RUNTIME
 from langgraph.config import get_config
@@ -13,6 +15,13 @@ from langgraph.typing import ContextT
 def _no_op_stream_writer(_: Any) -> None: ...
 
 
+class _RuntimeOverrides(TypedDict, Generic[ContextT], total=False):
+    context: ContextT
+    store: BaseStore | None
+    stream_writer: StreamWriter
+    previous: Any
+
+
 @dataclass(**_DC_KWARGS)
 class Runtime(Generic[ContextT]):
     """Convenience class that bundles run-scoped context and graph configuration.
@@ -20,21 +29,42 @@ class Runtime(Generic[ContextT]):
     !!! version-added "Added in version 1.0.0."
     """
 
-    context: ContextT
+    context: ContextT = field(default=None)  # type: ignore[assignment]
     """Static context for the graph run, like user_id, db_conn, etc.
     
     Can also be thought of as 'run dependencies'."""
 
-    store: BaseStore | None
+    store: BaseStore | None = field(default=None)
     """Store for the graph run, enabling persistence and memory."""
 
-    stream_writer: StreamWriter
+    stream_writer: StreamWriter = field(default=_no_op_stream_writer)
     """Function that writes to the custom stream."""
 
-    previous: Any | None
+    previous: Any = field(default=None)
     """The previous return value for the given thread.
     
-    Only available with the functional API when a checkpointer is provided."""
+    Only available with the functional API when a checkpointer is provided.
+    """
+
+    def merge(self, other: Runtime[ContextT]) -> Runtime[ContextT]:
+        """Merge two runtimes together.
+
+        If a value is not provided in the other runtime, the value from the current runtime is used.
+        """
+        return Runtime(
+            context=other.context or self.context,
+            store=other.store or self.store,
+            stream_writer=other.stream_writer
+            if other.stream_writer is not _no_op_stream_writer
+            else self.stream_writer,
+            previous=other.previous or self.previous,
+        )
+
+    def override(
+        self, **overrides: Unpack[_RuntimeOverrides[ContextT]]
+    ) -> Runtime[ContextT]:
+        """Replace the runtime with a new runtime with the given overrides."""
+        return replace(self, **overrides)
 
 
 DEFAULT_RUNTIME = Runtime(
