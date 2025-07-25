1491f30a07123cd460c4ec41d98c4180a1afd88a
Sydney Runkle <sydneymarierunkle@gmail.com>
2025-07-25 16:23:28 -0400

    ensure config also

--- libs/langgraph/langgraph/utils/config.py
+++ libs/langgraph/langgraph/utils/config.py
@@ -1,3 +1,4 @@
 """Backwards compat imports for config utilities, to be removed in v1."""
 
+from langgraph._internal._config import ensure_config  # noqa: F401
 from langgraph.config import get_config, get_store  # noqa: F401
