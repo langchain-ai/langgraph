176eaafbab5a64977ee204320329dc448eed2304
Hunter Lovell <hunter@hntrl.io>
2025-07-28 02:08:18 -0700

    chore: setup codeblock extraction

---
+++ docs/_scripts/js_translation/codeblocks/eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+]);
