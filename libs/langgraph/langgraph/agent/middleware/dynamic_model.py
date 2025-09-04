120d34303d5ad442f53f2e1d5428d1de65ba2e1f
Sydney Runkle <sydneymarierunkle@gmail.com>
2025-09-04 09:59:45 -0400

    adding model calls

---
+++ libs/langgraph/langgraph/agent/middleware/dynamic_model.py
@@ -0,0 +1,28 @@
+from langgraph.agent.types import AgentState, AgentUpdate, AgentJump, AgentMiddleware
+from langgraph.agent.types import ModelRequest
+from typing import Literal
+from dataclasses import dataclass
+from langchain_core.language_models.chat_models import BaseChatModel
+
+
+class DynamicModelMiddleware(AgentMiddleware):
+    """Selects different models based on task complexity"""
+
+    def __init__(
+        self,
+        basic_model: BaseChatModel,
+        complex_model: BaseChatModel,
+        message_threshold: int = 5,
+    ):
+        self.basic_model = basic_model
+        self.complex_model = complex_model
+        self.message_threshold = message_threshold
+
+    def modify_model_request(
+        self, request: ModelRequest, state: AgentState
+    ) -> ModelRequest:
+        if len(state.messages) > self.message_threshold:
+            request.model = self.complex_model
+        else:
+            request.model = self.basic_model
+        return request
