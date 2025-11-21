30e95ccefd645509741ede8d67c4fc4fe1d620da
Connor Braa <cwlbraa@langchain.dev>
2025-11-21 13:25:01 -0800

    chore(sdk-py): bump version to 0.2.10 for encryption feature

--- libs/sdk-py/langgraph_sdk/__init__.py
+++ libs/sdk-py/langgraph_sdk/__init__.py
@@ -3,6 +3,6 @@ from langgraph_sdk.client import get_client, get_sync_client
 from langgraph_sdk.encryption import Encrypt
 from langgraph_sdk.encryption.types import EncryptionContext
 
-__version__ = "0.2.9"
+__version__ = "0.2.10"
 
 __all__ = ["Auth", "Encrypt", "EncryptionContext", "get_client", "get_sync_client"]
