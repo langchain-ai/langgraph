d4a4182fd63fb9c44c102dbba0e3ecd2249de4c2
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2026-01-12 16:12:25 -0800

    chore: Better error messages

--- libs/sdk-py/langgraph_sdk/__init__.py
+++ libs/sdk-py/langgraph_sdk/__init__.py
@@ -3,6 +3,6 @@ from langgraph_sdk.client import get_client, get_sync_client
 from langgraph_sdk.encryption import Encryption
 from langgraph_sdk.encryption.types import EncryptionContext
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 __all__ = ["Auth", "Encryption", "EncryptionContext", "get_client", "get_sync_client"]
