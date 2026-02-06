fe084eef184e172d4db149963a9912a3263676cd
rafid saad <rafid.saad@langchain.dev>
2026-02-05 16:31:10 -0800

    Release python sdk

--- libs/sdk-py/langgraph_sdk/__init__.py
+++ libs/sdk-py/langgraph_sdk/__init__.py
@@ -3,6 +3,6 @@ from langgraph_sdk.client import get_client, get_sync_client
 from langgraph_sdk.encryption import Encryption
 from langgraph_sdk.encryption.types import EncryptionContext
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 
 __all__ = ["Auth", "Encryption", "EncryptionContext", "get_client", "get_sync_client"]
