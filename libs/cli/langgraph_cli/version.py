6300a6ce4c25fa9f1ccdfae084cb10a3a4ff6c71
alonn <alon.naor22@gmail.com>
2026-02-21 18:31:23 +0200

    perf: remove use of importlib.metadata for version access
    Replace runtime importlib.metadata.version() lookups with hardcoded
    version strings in langgraph and langgraph-cli. Use hatch dynamic
    versioning to read the version from source at build time.
    
    Closes #5040

--- libs/cli/langgraph_cli/version.py
+++ libs/cli/langgraph_cli/version.py
@@ -1,10 +1,3 @@
 """Main entrypoint into package."""
 
-from importlib import metadata
-
-try:
-    __version__ = metadata.version(__package__)
-except metadata.PackageNotFoundError:
-    # Case where package metadata is not available.
-    __version__ = ""
-del metadata  # optional, avoids polluting the results of dir(__package__)
+from langgraph_cli import __version__
