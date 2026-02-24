94014d14cd61c703bb8b91e840ce5fef859ce3c8
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2026-02-24 10:35:35 -0800

    release(sdk-py): 0.3.9

--- libs/sdk-py/langgraph_sdk/__init__.py
+++ libs/sdk-py/langgraph_sdk/__init__.py
@@ -3,6 +3,6 @@ from langgraph_sdk.client import get_client, get_sync_client
 from langgraph_sdk.encryption import Encryption
 from langgraph_sdk.encryption.types import EncryptionContext
 
-__version__ = "0.3.8"
+__version__ = "0.3.9"
 
 __all__ = ["Auth", "Encryption", "EncryptionContext", "get_client", "get_sync_client"]
