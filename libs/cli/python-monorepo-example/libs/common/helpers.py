3deb874d74dc54a93c1dd80008b780a6c4dba7c1
Sudhanwa <sudhanwamd@gmail.com>
2026-06-19 16:19:42 +0530

    fix: resolve issue #7814

--- libs/cli/python-monorepo-example/libs/common/helpers.py
+++ libs/cli/python-monorepo-example/libs/common/helpers.py
@@ -1,6 +1,11 @@
 """Common helper functions."""
 
 
+import platform
+
 def get_common_prefix() -> str:
     """Get a common prefix for messages."""
-    return "[COMMON]"
+    if platform.system() == "Windows":
+        return "[WINDOWS]"
+    else:
+        return "[COMMON]"
