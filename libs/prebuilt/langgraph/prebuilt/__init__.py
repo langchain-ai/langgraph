a3c7b56e0fb25417acaa4ba622286c27fb07db11
Elaheh Ghorbani <ellie@mac.home>
2026-07-06 14:27:31 -0400

    feat(prebuilt): expose additional tool middleware functions in the API

--- libs/prebuilt/langgraph/prebuilt/__init__.py
+++ libs/prebuilt/langgraph/prebuilt/__init__.py
@@ -2,6 +2,13 @@
 
 from langgraph.prebuilt._tool_call_transformer import ToolCallTransformer
 from langgraph.prebuilt.chat_agent_executor import create_react_agent
+from langgraph.prebuilt.tool_middleware import (
+    build_tool_call_key,
+    chain_async_tool_wrappers,
+    chain_tool_wrappers,
+    deduplicate_tool_calls,
+    deduplicate_tool_calls_in_state,
+)
 from langgraph.prebuilt.tool_node import (
     InjectedState,
     InjectedStore,
@@ -20,4 +27,9 @@ __all__ = [
     "InjectedState",
     "InjectedStore",
     "ToolRuntime",
+    "build_tool_call_key",
+    "chain_async_tool_wrappers",
+    "chain_tool_wrappers",
+    "deduplicate_tool_calls",
+    "deduplicate_tool_calls_in_state",
 ]
