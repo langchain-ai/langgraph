ef48df395bbe74dada58388de6711fa4ab392a31
botbikamordehai2-sketch <botbikamordehai2@gmail.com>
2026-08-11 10:08:39 +0000

    fix: flush pipeline before checkpoint operations (closes #5675)

--- libs/checkpoint-postgres/langgraph/checkpoint/postgres/aio.py
+++ libs/checkpoint-postgres/langgraph/checkpoint/postgres/aio.py
@@ -94,6 +94,8 @@ class AsyncPostgresSaver(BasePostgresSaver):
         already exist and runs database migrations. It MUST be called directly by the user
         the first time checkpointer is used.
         """
+        if self.pipe:
+            await self.pipe.sync()
         async with self._cursor() as cur:
             await cur.execute(self.MIGRATIONS[0])
             results = await cur.execute(
@@ -113,7 +115,7 @@ class AsyncPostgresSaver(BasePostgresSaver):
                 await cur.execute(
                     "INSERT INTO checkpoint_migrations (v) VALUES (%s)", (v,)
                 )
-        if self.pipe:
+            await self.pipe.sync() if self.pipe else None
             await self.pipe.sync()
 
     async def alist(
