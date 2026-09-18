fecbdef450d12e2da77b11719a397cd9a63ac3ce
Elior Nataf Lackritz <elior.nataflackritz@langchain.dev>
2026-09-18 15:06:59 -0400

    release(sdk-py): 0.4.5

--- libs/sdk-py/langgraph_sdk/__init__.py
+++ libs/sdk-py/langgraph_sdk/__init__.py
@@ -3,7 +3,7 @@ from langgraph_sdk.client import get_client, get_sync_client
 from langgraph_sdk.encryption import Encryption
 from langgraph_sdk.encryption.types import DecryptResult, EncryptionContext
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
 
 __all__ = [
     "Auth",
